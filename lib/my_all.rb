94e208fe25f8947e004dc187fca1d9ef93f799a4
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-16 00:22:47 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  while i < collection.length
-    yield(collection[i])
-    i += 1
-  end
-
+  if block_given?
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+  end  
 end
